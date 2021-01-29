971e10f28e505c34add252fecf85b9c9489060dc
Alex Tetervak <tetervak@sheridancollege.ca>
2021-01-29 11:16:01 -0500

    initial commit

---
+++ src/main/java/ca/tetervak/randcard/controller/RandCardController.java
@@ -0,0 +1,27 @@
+package ca.tetervak.randcard.controller;
+
+import ca.tetervak.randcard.domain.Card;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RandCardController {
+
+    private final Logger logger = LoggerFactory.getLogger(RandCardController.class);
+
+    @GetMapping(value={"/", "/Card"})
+    public String card(Model model){
+        logger.trace("card() is called");
+
+        Card card = new Card();
+        logger.debug("card = " + card);
+
+        model.addAttribute("card", new Card());
+        return "Card";
+    }
+
+
+}
---
+++ src/main/java/ca/tetervak/randcard/domain/Card.java
@@ -0,0 +1,34 @@
+// Alex Tetervak, Sheridan College, Ontario
+
+package ca.tetervak.randcard.domain;
+
+public class Card {
+
+    private final static String[] SUITS
+            = {"Spades", "Hearts", "Diamonds", "Clubs"};
+    private final static String[] FACES
+            = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven",
+            "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+
+    final private int number;
+    final private String name;
+
+    public Card(){
+        int rand = (int) (52 * Math.random());
+        name = FACES[rand % 13] + " of " + SUITS[rand / 13];
+        number = rand + 1;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
+}
